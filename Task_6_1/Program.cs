c927fcd5b352d3a9a0643a698ff9b287ff4c4ff2
Татьяна <veselovskayt@mail.ru>
2021-12-02 16:22:11 +0700

    Добавьте файлы проекта.

---
+++ Task_6_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words, result;
+            Console.WriteLine("Введите предложение");
+            words = Console.ReadLine();
+            string[] split = words.Split(' ');
+            result = split[0];
+
+            foreach (string s in split)
+            {
+                if (s.Trim() != "")
+                {
+                    if (result.Length < s.Length) result = s;
+                }
+            }
+            Console.WriteLine("Самое длинное слово - " + result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_6_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words, result, result_back;
+
+            Console.WriteLine("Введите предложение");
+            words = Console.ReadLine();
+
+            string[] split = words.Split(' ');
+
+            result = "";
+            foreach (string s in split)
+            {
+                if (s.Trim() != "")
+                {
+                    result = result + s;
+                }
+            }
+            char[] reverse = result.Reverse().ToArray();
+
+            result_back = "";
+            foreach (Char ch in reverse)
+            {
+                result_back = result_back + ch;
+
+            }
+
+            if (string.Compare(result, result_back, true) == 0)
+                Console.WriteLine("Строка палиндром ");
+            else Console.WriteLine("Строка не палиндром ");
+            Console.ReadKey();
+        }
+    }
+}
